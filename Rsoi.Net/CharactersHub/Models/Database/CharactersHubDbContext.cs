ef19f844e1823ee80373151c5a40bbe9bd84c0dc
Святослав Сычев <rk474@yandex.ru>
2019-09-05 17:51:14 +0300

    #1 add DbContext

---
+++ Rsoi.Net/CharactersHub/Models/Database/CharactersHubDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CharactersHub.Models.Database
+{
+    public class CharactersHubDbContext : DbContext
+    {
+        public CharactersHubDbContext(DbContextOptions<CharactersHubDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Character> Characters { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Character>(entity =>
+                entity.HasKey(c => c.Id)
+            );
+        }
+    }
+}
