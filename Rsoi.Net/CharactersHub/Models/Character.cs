daabcc69aaac4526c7fe1dd6921568279036e16e
Святослав Сычев <rk474@yandex.ru>
2019-09-05 17:50:21 +0300

    #1 add character model

---
+++ Rsoi.Net/CharactersHub/Models/Character.cs
@@ -0,0 +1,13 @@
+﻿namespace CharactersHub.Models
+{
+    public class Character
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Level { get; set; }
+
+        public bool IsMale { get; set; }
+    }
+}
